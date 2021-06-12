aa21c50d01fd9e2b32c79794e10dc916bb862aa5
Ricardo Passinho <passocurto@gmail.com>
2021-06-12 20:31:30 -0300

    restante.

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,37 +1,35 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-import { selectDirectorySections } from '../../redux/directory/directory.selectors';
-
-import './footer.styles.scss';
+import { selectDirectorySections } from "../../redux/directory/directory.selectors";
 
 
+import {foot} from './footer.styles';
 
-const Footer = ({ sections }) => ( 
-            <div className='footer'>
-                <div>
-                    <center className='clothers'>
-                            {
-                                sections.map(({ id, title}) => (
-                                    <a href={'/shop/' + title.toLowerCase()} className='clothers' key={id} > { title } </a> ) 
-                                ) 
-                            }
-                    </center>
-                </div>
-                <div className='developer'>
-                    <span> Developer Ricardo Passinho </span>
-                    <br />
-                    <span> Brazil - 2021 </span>
-                </div>
-            </div>
-    )
-
+const Footer = ({ sections }) => (
+  <foot>
+    <center>
+      <clothed>
+        {sections.map(({ id, title }) => (
+          <linkar href={"/shop/" + title.toLowerCase()} key={id}>
+            {" " + title + " "}
+          </linkar>
+        ))}
+      </clothed>
+      <br />
+      <developer>
+        <span> Developer Ricardo Passinho </span>
+        <br />
+        <span> Brazil - 2021 </span>
+      </developer>
+    </center>
+  </foot>
+);
 
 const mapStateToProps = createStructuredSelector({
-    sections: selectDirectorySections
-    });
-     
+  sections: selectDirectorySections,
+});
 
-export default withRouter(connect(mapStateToProps)(Footer));   
+export default withRouter(connect(mapStateToProps)(Footer));
---
+++ src/components/footer/footer.styles.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const foot = styled.div`
+  height: 200px;
+  display: block;
+  justify-content: space-between;
+  flex-direction: column;
+  bottom: 0;
+  width: 100%;
+  display: grid;
+  text-align: center;
+`;
+
+export const clothed = styled.div`
+  height: 200px;
+  justify-content: space-between;
+  flex-direction: column;
+  bottom: 0;
+  width: 100%;
+`;
+
+
+export const developer = styled.div`
+  margin: 0 0 25px 0;
+`;
